9952651bc88cba556eb8e4fd807da379b37ed676
Daud Ahmad <daudahmedkhokhar@gmail.com>
2018-07-09 12:24:46 +1000

    Picking up the port from env variable if set

--- client/constants.js
+++ client/constants.js
@@ -3,7 +3,8 @@ const moveSouth = "MS";
 const moveEast = "ME";
 const moveWest = "MW";
 const invalidCommandsMsg = "Invalid command, valid commands are: MN, MS, ME, MW, EXIT please try again!";
-const serviceUrl = "http://localhost:8080/room/";
+const PORT = process.env.PORT || 8080;
+const serviceUrl = `http://localhost:${PORT}/room/`;
 
 module.exports = {
   moveNorth: moveNorth,
